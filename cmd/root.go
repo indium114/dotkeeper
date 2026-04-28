a8655de16b7b784d05c17d3cb2094367ce572edd
indium114 <156162907+indium114@users.noreply.github.com>
2026-04-28 12:38:12 +0200

    ci: set up goreleaser

--- cmd/root.go
+++ cmd/root.go
@@ -8,14 +8,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "dotkeeper",
 	Short: "A simple, flexible symlink farm tool",
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := fang.Execute(context.Background(), rootCmd)
 	if err != nil {
